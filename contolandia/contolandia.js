b9c77dbf4df4d6a21a8f6eec8059424ff3fbed5e
Melissa Fernandes <87154023+mellunar@users.noreply.github.com>
2021-08-09 11:51:24 -0300

    Update contolandia.js

--- contolandia/contolandia.js
+++ contolandia/contolandia.js
@@ -1,14 +1,17 @@
 window.onload = function () {
     window.onscroll = function() {
-        const jstn = document.getElementById("jstn")
-        const logo = document.getElementById("logo")
+        const jstn = document.getElementById("jstn");
+        const logo = document.getElementById("logo");
+        const ddlist = document.getElementsByClassName("ddlist");
         if (window.scrollY > 150) {
             jstn.classList.add("tnscroll")
             logo.classList.add("logoscroll")
+            for (i = 0; i < ddlist.length; i++) {ddlist[i].classList.add("ddls")}
         }
         else {
             jstn.classList.remove("tnscroll")
             logo.classList.remove("logoscroll")
+            for (i = 0; i < ddlist.length; i++) {ddlist[i].classList.remove("ddls")}
         }
     };
 };
