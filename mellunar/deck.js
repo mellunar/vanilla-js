2b853d8c5d48f4fd65e30c02d3ac94cf26125399
Melissa Fernandes <87154023+mellunar@users.noreply.github.com>
2021-08-22 21:01:19 -0300

    Add files via upload

---
+++ mellunar/deck.js
@@ -0,0 +1,112 @@
+deck = [
+    {
+        img: `https://i.imgur.com/4mZnwKT.jpg`,
+        titulo: "O Tolo",
+        texto: "Representa começos, inocência, espontaneidade e consciência limpa."
+    },
+    {
+        img: `https://i.imgur.com/H9n69dO.jpg`,
+        titulo: "O Mago",
+        texto: "Representa auto-confiança, criatividade e força de vontade."
+    },
+    {
+        img: `https://i.imgur.com/YElKJbs.jpg`,
+        titulo: "A Sacerdotisa",
+        texto: "Representa mistério, paciência, compreensão e intuição."
+    },
+    {
+        img: `https://i.imgur.com/DqhQ4WP.jpg`,
+        titulo: "A Imperatriz",
+        texto: "Representa feminilidade, crescimento, fertilidade e semeadura, tornar algo real."
+    },
+    {
+        img: `https://i.imgur.com/5pBIHrd.jpg`,
+        titulo: "O Imperador",
+        texto: "Representa liderança, autoridade, poder e a incansável busca pelo objetivo."
+    },
+    {
+        img: `https://i.imgur.com/c9Ur3r3.jpg`,
+        titulo: "O Hierofante",
+        texto: "Representa lições práticas relacionadas a lei natural, estudo e a masterização por ele alcançada."
+    },
+    {
+        img: `https://i.imgur.com/GwKwU2N.jpg`,
+        titulo: "Os Amantes",
+        texto: "Representa a necessidade de tomar uma decisão, escolher algo em um caminho sem retorno."
+    },
+    {
+        img: `https://i.imgur.com/CYYQVyd.jpg`,
+        titulo: "O Carro",
+        texto: "Representa força, determinação e o triunfo de superar obstáculos."
+    },
+    {
+        img: `https://i.imgur.com/yIaGrHa.jpg`,
+        titulo: "A Força",
+        texto: "Representa disciplina interior e controle, a luta interna para superar um obstáculo."
+    },
+    {
+        img: `https://i.imgur.com/hBPHVdd.jpg`,
+        titulo: "O Eremita",
+        texto: "Representa introversão, reflexão interior e autodescoberta."
+    },
+    {
+        img: `https://i.imgur.com/mr4vipf.jpg`,
+        titulo: "A Roda da Fortuna",
+        texto: "Representa uma mudança inesperada, o ciclo de altos e baixos da vida."
+    },
+    {
+        img: `https://i.imgur.com/h0IeNqz.jpg`,
+        titulo: "Justiça",
+        texto: "Representa equilíbrio, responsabilidade e colher o que plantou, karma."
+    },
+    {
+        img: `https://i.imgur.com/OeQA3ix.jpg`,
+        titulo: "O Enforcado",
+        texto: "Representa crise, estagnação e a consequência, que traz consigo um novo ponto de vista."
+    },
+    {
+        img: `https://i.imgur.com/mXiOpHg.jpg`,
+        titulo: "A Morte",
+        texto: "Representa recomeços, deixar o passado para traz e seguir em frente."
+    },
+    {
+        img: `https://i.imgur.com/7wAfv1Y.jpg`,
+        titulo: "A Temperança",
+        texto: "Representa moderação, evolução através da maturidade e equilíbrio, evitando extremos."
+    },
+    {
+        img: `https://i.imgur.com/6GqDuzo.jpg`,
+        titulo: "O Diabo",
+        texto: "Representa ambição, desejo, ilusão e a prisão e/ou vínculo a algo ou alguém."
+    },
+    {
+        img: `https://i.imgur.com/DAPlOFb.jpg`,
+        titulo: "A Torre",
+        texto: "Representa perigo, ruína e uma mudança negativa."
+    },
+    {
+        img: `https://i.imgur.com/QMBjqht.jpg`,
+        titulo: "A Estrela",
+        texto: "Representa esperança, otimismo e transcendência."
+    },
+    {
+        img: `https://i.imgur.com/rWVhV3Z.jpg`,
+        titulo: "A Lua",
+        texto: "Representa ilusão, falta de clareza, decepção e confusão."
+    },
+    {
+        img: `https://i.imgur.com/mfjU1qt.jpg`,
+        titulo: "O Sol",
+        texto: "Representa prosperidade, alegria, clareza de mente e entusiasmo."
+    },
+    {
+        img: `https://i.imgur.com/N7o08Ot.jpg`,
+        titulo: "O Julgamento",
+        texto: "Representa renovação, ascensão, despertar, cura e libertação."
+    },
+    {
+        img: `https://i.imgur.com/mExoId7.jpg`,
+        titulo: "O Mundo",
+        texto: "Representa desfecho, o fim de um ciclo e/ou fim de uma jornada."
+    }
+];
